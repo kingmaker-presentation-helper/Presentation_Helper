a7032191219c2714a513f5dee1e66bf9a296b5c3
Jihae <62400658+easyssun@users.noreply.github.com>
2023-11-14 13:28:11 +0900

    Update extract_keyword.py
    modify text post processing

--- extract_keyword.py
+++ extract_keyword.py
@@ -109,28 +109,45 @@ def extract_keyword(text):
     kw_model = KeyBERT(model)
     kiwi = Kiwi()
     keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words=None, top_n=50)
+    print(keywords)
 
     for keyword in keywords:
+        if keyword == "":
+            continue
         # keyword_list.append(keyword[0])
         result = kiwi.analyze(keyword[0])
         keyword_str = ''
 
         for idx, (token, pos, _, _ )in enumerate(result[0][0]):
-            # print(token, pos, end =",  ")
+            # print(token, pos)
 
             # '갑자기'와 같은 부사가 중간에 나오면 그 앞까지만 사용
-            if pos.startswith('MAG'):
-                break
+            # if pos.startswith('MAG'):
+            #     break
             # 처벌'하'어야와 같이 중간에 동사 파생 접미사가 나오면 그 앞까지만 사용
-            if pos.startswith('XSV'):
+            if pos.startswith('XSV') or pos.startswith('XSA') or pos.startswith('XSM'):
                 break
             # 조사가 단어 중간에 나오면 그 앞까지만 사용
             if pos.startswith('J'):
                 if idx != 0 or idx != len(result[0][0]):
                     break
                 # 만약 조사가 단어 앞이나 끝에 나오면 조사는 결과에 포함하지 않음.
-
+            # 만약 대명사로 시작하면, 사용 X
+            if pos.startswith('NP'):
+                keyword_str = ""
+                break
+            # 어
+            if pos.startswith('EP') or pos.startswith('EF') or pos.startswith('EC'):
+                keyword_str = ""
+                break
+            # 부사는 사용 X
+            if pos.startswith("MA"):
+                break
             keyword_str = keyword_str + token
+        
+        # 결과가 ""이면 키워드로 저장하지 않기
+        if keyword_str == "":
+            continue
 
         keyword_list.append(keyword_str)
 
@@ -141,8 +158,47 @@ def extract_keyword(text):
     return delete_included_word3(keyword_list.copy())
 
 
-with open("test.txt", "r", encoding='UTF8') as file:
+
+# NP만 사용
+def extract_keyword2(text):
+    keyword_list = []
+
+    model = BertModel.from_pretrained('skt/kobert-base-v1')
+    # model = BertModel.from_pretrained('xlm-roberta-large')
+    
+    kw_model = KeyBERT(model)
+    kiwi = Kiwi()
+    keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words=None, top_n=50)
+    print(keywords)
+
+    for keyword in keywords:
+        result = kiwi.analyze(keyword[0])
+        keyword_str = ''
+
+        for _, (token, pos, _, _ )in enumerate(result[0][0]):
+            # print(token, pos,end=" | ")
+            if pos.startswith("SN"):
+                keyword_str = keyword_str + token
+            if pos.startswith("NN") and not(pos.startswith("NNB")): 
+                keyword_str = keyword_str + token
+            if keyword_str != "" and pos.startswith("XSN"):
+                keyword_str = keyword_str + token
+        # 결과가 ""이면 키워드로 저장하지 않기
+        if keyword_str == "":
+            continue
+
+        keyword_list.append(keyword_str)
+
+    # print(delete_included_word3(keyword_list.copy()))
+    
+    # return keyword_list
+
+    return delete_included_word3(keyword_list.copy())
+
+
+with open("입학식_연설_preprocessed_text.txt", "r", encoding='UTF8') as file:
     text = file.read()
 # print(text)
 
-print(extract_keyword(text))
+print()
+print(extract_keyword2(text))
